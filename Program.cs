408c796b25eb1ce38ade58a2d71e3dfa2e7861a9
gunmentoy <tomnguyen0443@gmail.com>
2026-03-27 12:20:30 -0700

    Auto-migrate database on startup

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
     SeedData.Initialize(services);
 }
 
